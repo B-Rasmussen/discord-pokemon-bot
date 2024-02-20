133eab6614fa4a79866acd7461f1f5fbb30a50ec
B-Rasmussen <brandonrasmussen91@gmail.com>
2024-02-19 18:04:49 -0800

    get pokemon type and return strength and weakness of that typing

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 const Discord = require("discord.js");
 const fetch = require("node-fetch");
 // ==============
-// TODO: MOVE POKEMON URL STUFF TO SEPARATE FILE
+//  TODO:
+//      - GET ALL STRENGTHS AND WEAKNESSES
+//      - MOVE POKEMON URL STUFF TO SEPARATE FILE
+//      - CLEAN UP AND ORGANIZE INTO INDEPENDENT FILES
 // ==============
 const pokemonBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
+const pokemonStrengthWeaknessUrl = "https://pokeapi.co/api/v2/type/";
 require("dotenv").config();
+
+// const slash = new Discord.SlashCommandBuilder().setName('pokemon').setDescription('gets stats about a pokemon')
 
 const client = new Discord.Client({
     intents: [
@@ -19,14 +25,137 @@ const client = new Discord.Client({
 
 const BOT_TOKEN = process.env["BOT_TOKEN"];
 
-function getPokemonInfo( pokemon ) {
-    return fetch(`${pokemonBaseUrl}${pokemon}`)
+function getPokemonStrengths(typing) {
+    return fetch(`${pokemonStrengthWeaknessUrl}${typing}`)
         .then((res) => {
             return res.json();
         })
         .then((data) => {
-            console.log("data: ", data);
-            return "checking...";
+            // console.log("pokemon stregth/weaknees: ", data.damage_relations);
+            const typingStrengths = JSON.stringify(
+                data.damage_relations.double_damage_to[0].name
+            ).replace(/\"/g, "");
+            const typingHalfDmgTo = JSON.stringify(
+                data.damage_relations.half_damage_to[0].name
+            ).replace(/\"/g, "");
+            const typingWeaknesses = JSON.stringify(
+                data.damage_relations.double_damage_from[0].name
+            ).replace(/\"/g, "");
+            const typingHalfDmgFrom = JSON.stringify(
+                data.damage_relations.half_damage_from[0].name
+            ).replace(/\"/g, "");
+            
+            console.log('double damage to: ', typingStrengths);
+            console.log('double damage from: ', typingWeaknesses);
+            console.log('half damage from: ', typingHalfDmgFrom);
+            console.log('half damage to: ', typingHalfDmgTo);
+
+            return (
+                "\n" +
+                "Deals double damage to: " +
+                typingStrengths +
+                "\n" +
+                "Takes double damage from: " +
+                typingWeaknesses +
+                "\n" +
+                "Takes half damage to: " +
+                typingHalfDmgFrom +
+                "\n" +
+                "Deals half damage to: " +
+                typingHalfDmgTo
+            );
+        });
+}
+
+function getPokemonInfo(pokemon) {
+    return fetch(`${pokemonBaseUrl}${pokemon}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then(async (data) => {
+            // console.log("data: ", data);
+            const type = JSON.stringify(data.types[0].type.name).replace(
+                /\"/g,
+                ""
+            );
+            const formattedType = "type: " + type;
+
+            // console.log("type: ", type);
+
+            switch (type) {
+                case "normal":
+                    var type_id = "1";
+                    break;
+                case "fighting":
+                    var type_id = "2";
+                    break;
+                case "flying":
+                    var type_id = "3";
+                    break;
+                case "poison":
+                    var type_id = "4";
+                    break;
+                case "ground":
+                    var type_id = "5";
+                    break;
+                case "rock":
+                    var type_id = "6";
+                    break;
+                case "bug":
+                    var type_id = "7";
+                    break;
+                case "ghost":
+                    var type_id = "8";
+                    break;
+                case "steel":
+                    var type_id = "9";
+                    break;
+                case "fire":
+                    var type_id = "10";
+                    break;
+                case "water":
+                    var type_id = "11";
+                    break;
+                case "grass":
+                    var type_id = "12";
+                    break;
+                case "electric":
+                    var type_id = "13";
+                    break;
+                case "psychic":
+                    var type_id = "14";
+                    break;
+                case "ice":
+                    var type_id = "15";
+                    break;
+                case "dragon":
+                    var type_id = "16";
+                    break;
+                case "dark":
+                    var type_id = "17";
+                    break;
+                case "fairy":
+                    var type_id = "18";
+                    break;
+                case "shadow":
+                    var type_id = "10002";
+                    break;
+                default:
+                    // DEFAULTS TO THE UNKNOWN TYPING IN https://pokeapi.co/api/v2/type/
+                    var type_id = "10001";
+                    break;
+            }
+            console.log("type id: ", type_id);
+
+            const pokemonStrength = await getPokemonStrengths(type);
+
+            return (
+                "checking for... " +
+                pokemon +
+                "\n" +
+                formattedType +
+                pokemonStrength
+            );
         });
 }
 
@@ -35,9 +164,13 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (msg) => {
+    // console.log('msg info: ', msg);
+    // const pokemon = msg.content.split(" ");
     if (msg.author.bot) return;
-    if (msg.content === "pokemon") {
-        getPokemonInfo("ditto").then(pokemonInfo => msg.channel.send(pokemonInfo))
+    if (msg.content.includes("pokemon")) {
+        getPokemonInfo("charmander").then((pokemonInfo) =>
+            msg.channel.send(pokemonInfo)
+        );
     }
 });
 
